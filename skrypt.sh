aa950962a0f49e5232cb826bc93e6e2dac57ca25
Julia Flaszynska <julia.flaszynska.99@gmail.com>
2024-06-29 21:22:53 +0200

    added alternative options

--- skrypt.sh
+++ skrypt.sh
@@ -3,17 +3,17 @@
 OPTION=$1
 
 case $OPTION in
-    --date)
+    -d|--date)
         date
         ;;
-    --logs)
+    -l|--logs)
         NUM=$2
         for ((i=1; i<=$NUM; ++i)); do
             FILENAME="log$i.txt"
             echo "$FILENAME \nskrypt.sh \n$(date)" >> $FILENAME
         done
         ;;
-    --help)
+    -h|--help)
         echo "Possible options are:"
         echo "--date :\t prints out the current date"
         echo "--logs x :\t creates x files titled log_number.txt; argument is optional"
